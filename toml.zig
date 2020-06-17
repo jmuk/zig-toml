3eabe07430a70020ddd4a98699e0380df2e57c3c
Jun Mukai <jun.mukai@gmail.com>
2020-06-16 21:10:51 -0700

    Support of simple key = value pattern
    This is not exactly as Toml's spec, but this would be a good
    start.

--- toml.zig
+++ toml.zig
@@ -2,7 +2,12 @@ const std = @import("std");
 const testing = std.testing;
 const expect = testing.expect;
 
-pub const Node = struct {
+pub const Node = union {
+    String: []const u8
+};
+
+pub const ParseError = error {
+    FailedToParse,
 };
 
 pub const Parser = struct {
@@ -14,8 +19,52 @@ pub const Parser = struct {
         return parser;
     }
 
-    pub fn parse(self: *Parser, input: []u8) !std.StringHashMap(*Node) {
-        return std.StringHashMap(*Node).init(self.allocator);
+    fn skipSpaces(self: *Parser, input: []const u8, offset: usize) usize {
+        var i: usize = offset;
+        while (i < input.len and (input[i] == ' ' or input[i] == '\t')) : (i+=1) {}
+        return i;
+    }
+
+    const tokenResult = struct {
+        token: []const u8,
+        offset: usize
+    };
+    fn parseToken(self: *Parser, input: []const u8, offset_in: usize) !tokenResult {
+        var offset = self.skipSpaces(input, offset_in);
+        var i: usize = offset;
+        while (i < input.len) : (i+=1) {
+            if (!((input[i] >= 'A' and input[i] <= 'Z') or (input[i] >= 'a' and input[i] <= 'z'))) {
+                break;
+            }
+        }
+        if (i == offset) {
+            return ParseError.FailedToParse;
+        }
+        return tokenResult{.token = input[offset..i], .offset = i};
+    }
+
+    fn parseAssign(self: *Parser, input: []const u8, offset_in: usize, data: *std.StringHashMap(Node)) !usize {
+        var key_result = try self.parseToken(input, offset_in);
+        var offset = self.skipSpaces(input, key_result.offset);
+        if (input[offset] != '=') {
+            return ParseError.FailedToParse;
+        }
+        var value_result = try self.parseToken(input, offset+1);
+        _ = try data.put(key_result.token, .{.str = value_result.token});
+        offset = self.skipSpaces(input, value_result.offset);
+        if (offset < input.len and input[offset] == '\n') {
+            offset+=1;
+        }
+        return offset;
+    }
+
+    pub fn parse(self: *Parser, input: []const u8) !std.StringHashMap(Node) {
+        var data = std.StringHashMap(Node).init(self.allocator);
+        var offset: usize = 0;
+        while (offset < input.len) loop: {
+            offset = try self.parseAssign(input, offset, &data);
+        }
+        return data;
     }
 };
 
@@ -27,4 +76,16 @@ test "empty" {
     defer allocator.destroy(parser);
     var parsed = try parser.parse("");
     expect(parsed.size == 0);
+}
+
+test "simple-kv" {
+    var tester = testing.LeakCountAllocator.init(std.heap.page_allocator);
+    defer tester.validate() catch {};
+    var allocator = &tester.allocator;
+    var parser = try Parser.init(allocator);
+    defer allocator.destroy(parser);
+    var parsed = try parser.parse(" \t foo\t=  bar   \n");
+    defer parsed.deinit();
+    expect(parsed.size == 1);
+    expect(std.mem.eql(u8, parsed.get("foo").?.value.str, "bar"));
 }
